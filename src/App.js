5731ef6927eb1e9a09b1d7d1d29a4d485babd6a8
DanialErfanian <erfaniiyanerfan@gmail.com>
2019-07-28 19:19:34 +0430

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="container col-7">
+                    <img src={logo} className="p-4 App-logo" alt="logo"/>
+                    <input className="Query-input form-control p-4 rounded w-75 container" type="text" name="query"/>
+                    <div className="d-flex justify-content-between container w-auto p-4">
+                        <input className="btn btn-light btn-lg btn-block w-50 container" type="submit" value="Search"/>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
