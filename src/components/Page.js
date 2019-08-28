4847646739b572a8d7010e4e7ab903502da9b679
DanialErfanian <erfaniiyanerfan@gmail.com>
2019-08-28 16:12:00 +0430

    Remove extra logs

--- src/components/Page.js
+++ src/components/Page.js
@@ -8,7 +8,6 @@ export class Page extends React.Component {
         ({title, link, content} = this.props.data);
         if (content.length > RESULT_CONTENT_SIZE)
             content = content.substring(0, RESULT_CONTENT_SIZE - 3) + "...";
-        console.log("content");
         return (
             <div className="page container p-1 col-10">
                 <h4 className="m-0 text-truncate">
